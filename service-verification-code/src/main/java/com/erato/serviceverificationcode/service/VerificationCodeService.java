0a6821e30614301c16145fe231d033c806fe0350
zhangYuan <zhangyuan@authing.cn>
2023-03-25 11:19:07 +0800

    service-verification-code. Structure of verification code generation

---
+++ service-verification-code/src/main/java/com/erato/serviceverificationcode/service/VerificationCodeService.java
@@ -0,0 +1,12 @@
+package com.erato.serviceverificationcode.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ZhangYuan
+ * @date 2023/3/25
+ */
+
+@Service
+public class VerificationCodeService {
+}
