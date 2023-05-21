e2be935ac2a450ffb43326a6f9e56ba3fab4d476
zhangYuan <zhangyuan@authing.cn>
2023-05-21 11:50:05 +0800

    feat: skeleton of service-price

---
+++ service-price/src/main/java/com/erato/ServicePriceApp.java
@@ -0,0 +1,18 @@
+package com.erato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class ServicePriceApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ServicePriceApp.class, args);
+    }
+}
