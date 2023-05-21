044abea00d03bbac8df599fb5fb62b5758a70fec
zhangYuan <zhangyuan@authing.cn>
2023-05-22 00:26:26 +0800

    feat: 新建api-driver

---
+++ api-driver/src/main/java/com/erato/apidriver/ApiDriverApplication.java
@@ -0,0 +1,13 @@
+package com.erato.apidriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDriverApplication.class, args);
+    }
+
+}
