aa5911c1a05cd604f89ed78a427cf2ef0b58498c
zhangYuan <zhangyuan@authing.cn>
2023-05-21 12:50:18 +0800

    feat: service-price 调用 service-map

---
+++ internal-common/src/main/java/com/erato/internalcommon/response/DirectionResponse.java
@@ -0,0 +1,7 @@
+package com.erato.internalcommon.response;
+
+import lombok.Data;
+
+@Data
+public class DirectionResponse {
+}
---
+++ service-price/src/main/java/com/erato/service/price/map/ServiceMapClient.java
@@ -0,0 +1,15 @@
+package com.erato.service.price.map;
+
+import com.erato.internalcommon.dto.ForecastPriceDTO;
+import com.erato.internalcommon.dto.ResponseResult;
+import com.erato.internalcommon.response.DirectionResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient("service-map")
+public interface ServiceMapClient {
+
+    @GetMapping("/direction/driving")
+    public ResponseResult<DirectionResponse> direction(@RequestBody ForecastPriceDTO forecastPriceDTO);
+}
