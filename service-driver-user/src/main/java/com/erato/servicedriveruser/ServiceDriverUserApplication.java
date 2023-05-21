626b35e5ae321156a2107de8b50a01e759faebb4
zhangYuan <zhangyuan@authing.cn>
2023-05-21 23:21:23 +0800

    feat: service-driver-user

---
+++ service-driver-user/src/main/java/com/erato/servicedriveruser/ServiceDriverUserApplication.java
@@ -0,0 +1,13 @@
+package com.erato.servicedriveruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDriverUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDriverUserApplication.class, args);
+    }
+
+}
