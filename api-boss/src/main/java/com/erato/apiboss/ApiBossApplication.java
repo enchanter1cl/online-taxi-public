d7edb18f3c280014cf9c346215c3a0ac8b004084
zhangYuan <zhangyuan@authing.cn>
2023-05-22 00:18:47 +0800

    feat: api-boss 添加司机

---
+++ api-boss/src/main/java/com/erato/apiboss/ApiBossApplication.java
@@ -0,0 +1,13 @@
+package com.erato.apiboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBossApplication.class, args);
+    }
+
+}
